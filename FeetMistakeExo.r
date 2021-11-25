c4fa9c5549880c066549e2fd01e40592e861c870
Aimseen <vincent.ribot@free.fr>
2021-11-25 11:35:36 +0100

    Organisation repo git + Exercice 1 au propre (passage de org a rstudio)

--- FeetMistakeExo.r
+++
@@ -1,6 +0,0 @@
-data <- read.csv("~/Documents/Etude/M2bis/SM/SMPE-Perso/data.csv", dec=",")
-plot(data,main="Number of mistakes mades by all kids in an elementary school\n related to their feet size",
-     xlab="Feet size", ylab="Number of mistakes")
-reg<-lm(NB_OF_MISTAKES ~ FEET_SIZE, data = data)
-abline(reg, col="red")
-legend(24, 20, legend="linear regression line", fill="red")
